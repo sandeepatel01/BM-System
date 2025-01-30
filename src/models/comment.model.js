bf8eabb07669de5d1d64e44b72ee7dae7cc55095
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-01-30 08:07:49 +0530

    Add comment model

---
+++ src/models/comment.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  blog: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Blog",
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+},
+  {
+    timestamps: true
+  }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
