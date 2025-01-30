8536e84c18d1c0a69d128beb497e0d321b4b9280
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-01-31 02:18:47 +0530

    Add all endpoints of blogs

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -22,8 +22,10 @@ app.use(cookieParser());
 
 
 import userRouter from "./routes/user.route.js";
+import blogRouter from "./routes/blog.route.js";
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/blogs", blogRouter);
 
 
 export { app } 
---
+++ src/routes/blog.route.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { addComment, assignBlogToEditor, createBlog, deleteBlog, deleteComment, getAllBlogs, getBlogById, updateBlog } from "../controllers/blog.controller";
+import { isAdmin, verifyJWT } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+// Public routes
+router.route("/all-blogs").get(getAllBlogs);
+router.route("/:blogId").get(getBlogById);
+
+// Secured routes
+router.route("/create-blog").post(verifyJWT, isAdmin, createBlog);
+router.route("/update-blog/:blogId").put(verifyJWT, isAdmin, updateBlog);
+router.route("/delete-blog/:blogId").delete(verifyJWT, isAdmin, deleteBlog);
+
+// Admin assigns blog to editor
+router.route("/assign").post(verifyJWT, isAdmin, assignBlogToEditor);
+
+// Comment routes
+router.route("/:blogId/comments").post(verifyJWT, addComment);
+router.route("/comments/:commentId").delete(verifyJWT, deleteComment);
+
+
+export default router
